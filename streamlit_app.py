7b41c43fca4486eb22c6944d282a822d5f0f9213
Reyan Andrea <reyanandre@gmail.com>
2025-12-15 11:55:44 +0700

    donnee

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ Streamlit Web Application - Modern UI
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pltpython 
 import seaborn as sns
 from tensorflow import keras
 import joblib
